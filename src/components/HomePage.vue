4184f8a36ca67c7765afa6f5facf7a196a206f31
DavideTarantino <davidetarantino2005@gmail.com>
2024-06-04 15:38:18 +0200

    basic data display

---
+++ src/components/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1>Dolci Disponibili</h1>
+    <ul>
+      <li v-for="dolce in dolci" :key="dolce.id">
+        {{ dolce.nome }} - €{{ dolce.prezzo }} - Quantità: {{ dolce.quantita }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      dolci: []
+    };
+  },
+  methods: {},
+  created() {
+    api.getDolci().then(data => {
+      console.log('Dati ricevuti nel componente:', data);
+      this.dolci = data})
+    }
+};
+</script>
+
+<style scoped>
+.home{
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  color: blue;
+}
+li {
+  padding: 10px 0;
+}
+</style>
---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3000',
+  withCredentials: false,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
+
+export default {
+  getDolci() {
+    return apiClient.get('/dolci')
+      .then(response => {
+        console.log('Dati ricevuti da API:', response.data);
+        return response.data; // Assicurati di restituire solo i dati
+      })
+      .catch(error => {
+        console.error('Errore nel recupero dei dati:', error);
+        throw error;
+      });
+  }
+};
